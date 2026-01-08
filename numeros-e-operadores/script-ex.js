608e5977f9941cf6721f64fb29cfb50b01970497
Mateus Oliveira <newsmateus@gamil.com>
2026-01-07 21:12:19 -0300

    adiciona arquivos HTML e JavaScript para a seção de números e operadores

---
+++ numeros-e-operadores/script-ex.js
@@ -0,0 +1,28 @@
+/*Ex 01 */
+let total = 35;
+
+/*Ex 02 */
+let nan01 = `Isso é um teste`;
+console.log(+nan01);
+
+let nan02 = `Mateus`;
+console.log(nan02 / 2);
+
+/*Ex 03*/
+let num = "200";
+let num2 = 50;
+
+console.log(+num + num2);
+
+/*Ex 04 */
+let incremento = 5;
+
+console.log(++incremento);
+
+/*Ex 05 */
+var numero = "80";
+var unidade = "kg";
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = numero / 2; // NaN (Not a Number)
+
+console.log(pesoPorDois);
---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,13 @@
+let teste = `21`;
+
+let num = +teste + 1;
+
+/*console.log(num);
+console.log(typeof num);*/
+
+let faculdade = true;
+
+let testeBoleano = +faculdade;
+
+console.log(testeBoleano);
+console.log(typeof testeBoleano);
